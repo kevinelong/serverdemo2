6d2a9f97861ea31ff4da2c6ba9f0a7f98049bf42
Kevin Ernest Long <kevinelong@gmail.com>
2022-11-22 14:27:08 +0000

    comments

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const express = require("express");
-const fs = require('fs');
+const express = require("express"); //WEB SERVER FRAMEWORK
+const fs = require('fs'); //FILE SYSTEM FOR "DATABASE"
 
-const app = express();
+const app = express(); //CREATE INSTANCE OF WEB SERVER
+//ADD THE JSON PLUGIN FOR THE SERVER
 app.use(express.json({extended: true, limit: '1mb'}));
 
-app.get('/',(req, res) => res.sendfile("index.html") );
+//MAP A ROUTE (Endpoint to Function, e.g. GET / => lambda)
+app.get('/', (req, res) => res.sendfile("index.html") );
 
+//MAP A ROUTE. (GET /products => lambda)
 app.get('/products',(req, res) => {
+  //READ JSON TEXT FILE, CONVERT TO DATA, THEN CONVERT TO JSON RESPONSE.
   res.json(JSON.parse(fs.readFileSync('products.json')))
 });
 
-app.listen(3000,() => {
-  console.log("Started on PORT 3000");
-});
+//BEGIN LISTENING FOR WEB REQUESTS ON PORT 3000
+app.listen(3000, () => console.log("Started on PORT 3000"));
